af7c91952c9b9689266292710ceb04f70c79703d
Sunny <sunnysinghhrana@gmail.com>
2026-07-25 23:07:22 +0530

    -testing

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,17 @@
 function Login() {
-    return <h1>Login Page</h1>;
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-slate-100">
+            <div className="bg-white p-10 rounded-xl shadow-lg">
+                <h1 className="text-3xl font-bold text-blue-600">
+                    Hydration Tracker
+                </h1>
+
+                <p className="mt-3 text-gray-500">
+                    Tailwind is working.
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default Login;

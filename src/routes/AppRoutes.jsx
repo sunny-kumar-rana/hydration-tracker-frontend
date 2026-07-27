83f70d7d6662ce4fb904625b7d15a0d99ac6fa90
Sunny <sunnysinghhrana@gmail.com>
2026-07-27 22:26:09 +0530

    -remove protected routes from AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,18 +7,66 @@ import Water from "../pages/Water/Water";
 import Statistics from "../pages/Statistics/Statistics";
 import Profile from "../pages/Profile/Profile";
 
+import ProtectedRoute from "../components/ProtectedRoute";
+
 function AppRoutes() {
+
     return (
+
         <BrowserRouter>
+
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/water" element={<Water />} />
-                <Route path="/statistics" element={<Statistics />} />
-                <Route path="/profile" element={<Profile />} />
+
+                <Route
+                    path="/"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
+
+                <Route
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/water"
+                    element={
+                        <ProtectedRoute>
+                            <Water />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/statistics"
+                    element={
+                        <ProtectedRoute>
+                            <Statistics />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
+
             </Routes>
+
         </BrowserRouter>
+
     );
 }
 

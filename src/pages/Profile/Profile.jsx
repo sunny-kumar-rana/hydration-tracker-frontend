3bee100aef1625add799f2e57d93beefc58590e2
Sunny <sunnysinghhrana@gmail.com>
2026-07-27 22:22:11 +0530

    -add dashboard layout in Profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,12 @@
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+
 function Profile() {
-    return <h1>Profile Page</h1>;
+    return (
+        <DashboardLayout>
+            <h1>Profile Page</h1>
+        </DashboardLayout>
+    );
 }
 
 export default Profile;

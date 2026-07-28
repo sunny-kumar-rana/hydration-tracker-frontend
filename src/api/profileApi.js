7650d462853f9c14ee6199dbb383581b43d092f1
Sunny <sunnysinghhrana@gmail.com>
2026-07-28 23:32:09 +0530

    -add profileApi.js

---
+++ src/api/profileApi.js
@@ -0,0 +1,8 @@
+import api from "./axios";
+
+export const getProfile = () => api.get("/profile");
+
+export const updateProfile = (data) => api.put("/profile", data);
+
+export const changePassword = (data) =>
+  api.put("/profile/change-password", data);

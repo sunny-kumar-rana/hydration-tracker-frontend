78ba422f8ef6a2f83f54a1e6227076e597d860fe
Sunny <sunnysinghhrana@gmail.com>
2026-07-31 23:53:00 +0530

    -add toasts instead of alerts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 
 import { AuthProvider } from "./context/AuthContext";
@@ -9,6 +10,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
     </AuthProvider>
   </React.StrictMode>
 );

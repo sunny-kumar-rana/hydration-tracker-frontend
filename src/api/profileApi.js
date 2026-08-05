70e533e5c7b5aad8c12855f440584d26050a3474
Sunny <sunnysinghhrana@gmail.com>
2026-08-05 23:44:22 +0530

    -add endpoints

--- src/api/profileApi.js
+++ src/api/profileApi.js
@@ -6,3 +6,7 @@ export const updateProfile = (data) => api.put("/profile", data);
 
 export const changePassword = (data) =>
   api.put("/profile/change-password", data);
+
+export const testEmail = () => api.post("/profile/test-email");
+
+export const testTelegram = () => api.post("/profile/test-telegram");

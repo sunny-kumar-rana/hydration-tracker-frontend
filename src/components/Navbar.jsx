4d67b51fb41324d30757c476195595b83c3950f8
Sunny <sunnysinghhrana@gmail.com>
2026-08-07 22:04:01 +0530

    -minor modification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,55 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { FaBell, FaUserCircle } from "react-icons/fa";
 
 function Navbar() {
 
-    const { logout } = useAuth();
-    const navigate = useNavigate();
 
-    function handleLogout() {
-        logout();
-        navigate("/");
-    }
+    const date = new Date().toLocaleDateString(undefined, {
+        weekday: "long",
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+    });
 
     return (
-        <header className="bg-blue-600 text-white px-8 py-4 flex justify-between items-center shadow">
 
-            <h1 className="text-xl font-bold">
-                💧 Hydration Tracker
-            </h1>
+        <header className="bg-slate-200 shadow-sm rounded-xl px-8 py-5 flex justify-between items-center">
 
-            <button
-                onClick={handleLogout}
-                className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded"
-            >
-                Logout
-            </button>
+            <div>
+
+
+                <h1 className="text-2xl font-bold text-blue-600">
+
+                    💧 Hydration Tracker
+
+                </h1>
+
+                <p className="text-gray-500 mt-5 ml-5">
+
+                    {date}
+
+                </p>
+
+            </div>
+
+            <div className="flex items-center gap-6">
+
+                <button className="relative">
+
+                    <FaBell className="text-2xl text-gray-600 hover:text-blue-600 transition" />
+
+                    <span className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full" />
+
+                </button>
+
+                <FaUserCircle className="text-4xl text-blue-600" />
+
+
+            </div>
 
         </header>
+
     );
+
 }
 
 export default Navbar;

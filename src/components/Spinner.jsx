86bbc91cd64f070cb6430f45a7294e5bd65e465f
Sunny <sunnysinghhrana@gmail.com>
2026-08-07 22:04:25 +0530

    -minor changes

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,25 @@
+function Spinner() {
+
+    return (
+
+        <div className="flex justify-center py-12">
+
+            <div
+
+                className="w-10
+h-10
+border-4
+border-blue-500
+border-t-transparent
+rounded-full
+animate-spin"
+
+            />
+
+        </div>
+
+    );
+
+}
+
+export default Spinner;
